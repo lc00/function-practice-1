cd680fc9868b49a8f59fd1f1ea8be1b69690568b
lc00 <leannecheung00@gmail.com>
2014-06-24 12:34:05 -0600

    through 8. sun

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 /*  5. toArray */
 
-function toArray(x,y,z){
+var toArray = function(x,y,z){
   return [x,y,z];
 }
 
-console.log(toArray(1,3,4));
+console.log(toArray(1,10,100));
+
+
+/* 6. startsWithA */
+
+var startsWith = function(word){
+	return word[1].toLowerCase() === "a";
+}
+
+console.log(startsWith("aardvark"));
+console.log(startsWith("bear"));
+
+
+/* 7. excite */
+
+var excite = function(word){
+	return word + "!!!";
+}
+
+console.log(excite("yes"));
+
+
+/* 8. sun */
+var sun = function(word){
+
+	return word.indexOf("sun") >= 0;
+
+}
+
+console.log(sun("yeah sun"));
+console.log(sun("abadasun"));
+
+console.log(sun("sun"));
+console.log(sun("Sun"));
